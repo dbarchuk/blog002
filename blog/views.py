289e5d568cf277a120806993568cc9a11a865fb8
dmytro barchuk <dmytro.barchuk@vilmate.com>
2022-11-12 11:40:58 +0200

    added views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic.list import ListView
 
-# Create your views here.
+from .models import Post
+
+
+def posts_list(request):
+    posts = Post.objects.all()
+    context = {
+        "posts": posts
+    }
+    return render(request, "posts_list.html", context)
+
+
+def post_detail(request, id):
+    # post = Post.objects.get(id=id)
+    post = get_object_or_404(Post, pk=id)
+    context = {
+        "post": post
+    }
+    return render(request, "posts_detail.html", context)
+
+
+class PostsListView(ListView):
+    model = Post
+    paginate_by = 2
+    # template_name = "posts_list.html"
+    # context_object_name = "posts"
