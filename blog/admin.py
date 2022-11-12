2a23dc10b1489199d73d0a5024940258060ac44f
dmytro barchuk <dmytro.barchuk@vilmate.com>
2022-11-12 11:00:11 +0200

    added blog functionality

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post, Comment, Image, Tag
+
+
+class ImageInLine(admin.TabularInline):
+    model = Image
+    extra = 1
+
+
+class CommentInLine(admin.StackedInline):
+    model = Comment
+    extra = 1
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    inlines = [ImageInLine, CommentInLine]
+    filter_horizontal = ["tags"]
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Tag(models.Model):
+    title = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField(default="", blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    date_cteation = models.DateField(auto_now_add=True)
+    created_by = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name="posts")
+    tags = models.ManyToManyField(Tag, blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Image(models.Model):
+    image = models.ImageField(upload_to="images/")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="images")
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    created_by = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name="comments")
+    comment = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
